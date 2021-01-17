e548987e16ba8df3e01223c7702ae13d5ed04ded
Haris Muhammad <haris@flip.app>
2021-01-17 12:28:39 -0500

    pass tests - chapter 2

--- exercises/chapter2/test/MySolutions.purs
+++ exercises/chapter2/test/MySolutions.purs
@@ -1,3 +1,14 @@
 module Test.MySolutions where
 
 import Prelude
+import Data.Int (rem)
+import Math (sqrt, pi)
+
+diagonal :: Number -> Number -> Number
+diagonal w h = sqrt (w * w + h * h)
+
+circleArea :: Number -> Number
+circleArea r = r * r * pi 
+
+leftoverCents :: Int -> Int
+leftoverCents c = rem c 100
